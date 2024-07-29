06b443631ae00f48700795012fcbda62f4fccce2
Muhammed Arshad T P <arshadtp89@gmail.com>
2024-07-30 01:31:57 +0530

    Linked list append prepend remove and insert

--- index.js
+++ index.js
@@ -44,21 +44,22 @@
 // class linkedList{
 //     constructor(){
 //         this.head=null
-//         this.tail=null
 //         this.size=0
 //     }
+// isEmpty() {                 // This method check if the list is empty or not
+//     return this.size === 0
+// }
 
 //     getSize(){
 //         return this.size
 //     }
 //     prepend(value){
 //         const node = new Node(value)
-//         if(this.head == null){
+//         if(this.isEmpty()){
 //             this.head=node
-//             this.tail=this.head
 //         }else{
-//         this.tail.next=node
-//         this.tail=node
+//         node.next=this.head
+//         this.head=node
 //         }
 //         this.size++
 //     }
@@ -95,20 +96,28 @@
 // class linkedList{
 //     constructor(){
 //         this.head=null
-//         this.tail=null
+//         this.size=0
 //     }
+
 //     getSize(){
 //         return this.size
+//     }
+// isEmpty() {                 // This method check if the list is empty or not
+//         return this.size === 0
 //     }
 
 //     append(value){
 //         const node = new Node(value)
-//         if(this.head==null){
+//         if(this.isEmpty()){
 //             this.head=node
 //         }else{
-//             this.tail.next=node
+//             let prve=this.head
+//          while(prve.next){
+ //             prve=prve.next
+//           }
+//           prve.next=node
+            
 //         }
-//         this.tail=node
 //         this.size++
 
 //     }
@@ -131,3 +140,146 @@
 // link.display()
 
 // console.log(link);
+
+
+// ============Insert=============//
+
+// class Node{
+//     constructor(value){
+//         this.value=value
+//         this.next=null
+//     }
+// }
+
+// class linkedList{
+//     constructor(){
+//         this.head=null
+//         this.size=0
+//     }
+//     isEmpty(){
+//         return this.size===0
+//     }
+//     prepend(value){
+//         const node = new Node(value)
+//         if(this.isEmpty()){
+//             this.head=node
+//         }else{
+//         node.next=this.head
+//         this.head=node
+//         }
+//         this.size++
+//     }
+
+//     insert(value,index){
+//         if(index<0||index>this.size){
+//             return
+//         }
+//         if(index===0){
+//             this.prepend(value)
+//         }else{
+//             const node =new Node(value)
+//             let prev =this.head
+//             for(let i=0;i<index-1;i++){
+//                 prev=prev.next
+//             }
+//             node.next=prev.next
+//             prev.next=node
+//         }
+//         this.size++
+//     }
+
+//     display(){
+//         let current =this.head
+//         while(current!=null){
+//             console.log(current.value);
+//             current = current.next
+//         }
+//     }
+// }
+
+// const item =new linkedList()
+
+
+// item.prepend(10)
+// item.prepend(20)
+// item.prepend(30)
+
+
+// item.insert(90,2)
+// item.insert(100,3)
+// item.display()
+
+// =================RemoveNode===============//
+
+// class Node{
+//     constructor(value){
+//         this.value=value
+//         this.next=null
+//     }
+// }
+
+// class linkedList{
+//     constructor(){
+//         this.head=null
+//         this.size=0
+//     }
+
+//     isempty(){
+//         return this.size===0
+//     }
+
+
+//     prepend(value){
+//         const node = new Node(value)
+//         if(this.isempty()){
+//             this.head=node
+//         }else{
+//             node.next=this.head
+//             this.head=node
+//         }
+//         this.size++
+//     }
+//     remove(index){
+//         if(index<0 || index>=this.size){
+//             return null
+//         }
+//         let RemoveNode
+//         if(index==0){
+//             RemoveNode=this.head
+//             this.head=this.head.next
+//         }else{
+//             let prev=this.head
+//             for(let i=0; i<index-1; i++){
+//                 prev=prev.next
+//             }
+//             RemoveNode=prev.next
+//             prev.next=RemoveNode.next
+//         }
+//         this.size--
+//         return RemoveNode.value
+//     }
+//     display(){
+//         let current = this.head
+//         while(current!=null){
+//             console.log(current.value)
+//             current=current.next
+//         }
+//     }
+// }
+
+// const link= new linkedList()
+
+// link.prepend(23)
+// link.prepend(34)
+// link.prepend(67)
+// link.prepend(10)
+
+// link.remove(0)
+
+// link.display()
+
+
+
+
+
+
