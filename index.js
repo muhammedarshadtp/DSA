83e53313880e3ac76a7805971032be1c20359bc2
Muhammed Arshad T P <arshadtp89@gmail.com>
2024-07-31 10:17:55 +0530

    linkedlist remove the value index insert search

--- index.js
+++ index.js
@@ -239,24 +239,45 @@
 //         }
 //         this.size++
 //     }
-//     remove(index){
-//         if(index<0 || index>=this.size){
+//     // remove(index){
+//     //     if(index<0 || index>=this.size){
+//     //         return null
+//     //     }
+//     //     let RemoveNode
+//     //     if(index==0){
+//     //         RemoveNode=this.head
+//     //         this.head=this.head.next
+//     //     }else{
+//     //         let prev=this.head
+//     //         for(let i=0; i<index-1; i++){
+//     //             prev=prev.next
+//     //         }
+//     //         RemoveNode=prev.next
+//     //         prev.next=RemoveNode.next
+//     //     }
+//     //     this.size--
+//     //     return RemoveNode.value
+//     // }
+//     remove(value){
+//         if(this.isempty()){
 //             return null
 //         }
-//         let RemoveNode
-//         if(index==0){
-//             RemoveNode=this.head
+//         if(this.head.value===value){
 //             this.head=this.head.next
+//             this.size--
+//             return value
 //         }else{
-//             let prev=this.head
-//             for(let i=0; i<index-1; i++){
+//             let prev = this.head
+//             while(prev.next && prev.next.value != value){
 //                 prev=prev.next
 //             }
-//             RemoveNode=prev.next
-//             prev.next=RemoveNode.next
+//             if(prev.next){
+//                 const remooves = prev.next
+//                 prev.next=remooves.next
+//                 this.size--
+//                 return value
+//             }
 //         }
-//         this.size--
-//         return RemoveNode.value
 //     }
 //     display(){
 //         let current = this.head
@@ -274,12 +295,112 @@
 // link.prepend(67)
 // link.prepend(10)
 
-// link.remove(0)
+// link.remove(34)
 
 // link.display()
 
 
+// ===========Search=========//
 
+class Node{
+    constructor(value){
+        this.value=value
+        this.next=null
+    }
+}
+
+class linkedList{
+    constructor(){
+        this.head=null
+        this.size=0
+    }
+    isempty(){
+        return this.size===0
+    }
+    append(value){
+        const node=new Node (value)
+        if(this.isempty()){
+         this.head=node
+        }else{
+            let prev=this.head
+            while(prev.next){
+                prev=prev.next
+            }
+            prev.next=node
+            
+        }
+        this.size++
+    }
+
+//     search(value){
+//         console.log(this.head,'length');
+//         if(this.isempty()){
+//             return -1
+//         }
+//         let i=0
+//         let curr = this.head
+//         while(curr){
+//         if(curr.value===value){
+//             return i
+//         }
+//         curr=curr.next
+//         i++
+//     }
+//     return -1
+//   }
+searchWithIndex(index){
+    if(index<0 || index >=this.size){
+        return -1
+    }
+    if(index===0){
+        return this.head.value
+    }else{
+        let prev=this.head
+        let count=0
+        while(prev){
+            if(count === index){
+                return prev.value
+            }
+            prev=prev.next
+            count++
+
+        }
+        return -1
+    }
+}
+
+reverce(value){
+    let prev=null
+    let current=this.head
+    while(current){
+        let next=current.next
+        current.next=prev
+        prev=current
+        current=next
+    }
+    this.head=prev
+}
+    
+    display(){
+
+        let current=this.head
+        while(current != null){
+            console.log(current.value);
+            current=current.next
+        }
+    }
+}
+
+const link= new linkedList()
+
+link.append(23)
+link.append(78)
+link.append(87)
+
+console.log(link.searchWithIndex(1),'val');
+// console.log(link.reverce(),"reverce kitty");
+link.reverce()
+link.display()
 
 
 
