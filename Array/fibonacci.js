c1e60ac44c46921007f4c15c82a0279bd7cc8605
Muhammed Arshad T P <arshadtp89@gmail.com>
2024-08-02 09:45:28 +0530

    RecursionFibonacci

--- Array/fibonacci.js
+++ Array/fibonacci.js
@@ -47,3 +47,58 @@ console.log(isPrime(82));
 console.log(isPrime(22));
 
 //====Big-O =O(n)
+
+
+// ========Power of Two========//
+
+
+function power(n){
+    if(n<1){
+        return false
+    }
+    while(n>1){
+        if(n % 2 !==0){
+            return false
+        }
+        n=n/2
+        return true
+    }
+}
+
+console.log(power(5),'power kitty');
+console.log(power(9));
+console.log(power(5));
+
+//======Big-O = o(logn)
+
+
+function ispoweroftwobite(n){
+    if(n<1){
+        return false
+    }
+    return (n & (n-1)) === 0
+}
+
+console.log(ispoweroftwobite(4),'=======');
+
+//=====Big-O=O(1)
+
+
+
+// ===========Recursion Fibonacci Sequence==========//
+
+function RecursionFibonacci(n){
+    if(n<2){
+        return n
+    }
+    return RecursionFibonacci(n-1) + RecursionFibonacci(n-2)
+}
+
+
+console.log("==========================");
+
+console.log(RecursionFibonacci(6));  //(8)
+console.log(RecursionFibonacci(8));  //(21)
+console.log(RecursionFibonacci(10)); //(55)
+console.log(RecursionFibonacci(4));  //(3)
+
