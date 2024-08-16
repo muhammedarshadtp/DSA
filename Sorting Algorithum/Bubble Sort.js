8527fbd876b55af32a8096f8078f10072a2b6833
Muhammed Arshad T P <arshadtp89@gmail.com>
2024-08-16 20:41:30 +0530

    InsertionSort parctice

--- Sorting Algorithum/Bubble Sort.js
+++ Sorting Algorithum/Bubble Sort.js
@@ -3,23 +3,26 @@
 // ==============Bubble sort================//
 
 
-function BubbleSort(arr){
-    let swapped;
+// function BubbleSort(arr){
+//     let swapped;
     
-    do{
-        swapped = false
-        for(let i=0; i<arr.length-1; i++){
-            if(arr[i] > arr[i+1]){
-                let temp=arr[i]
-                arr[i]=arr[i+1]
-                arr[i+1]=temp
-                swapped = true
-            }
-        }
-    } while(swapped)
-}
+//     do{
+//         swapped = false
+//         for(let i=0; i<arr.length-1; i++){
+//             if(arr[i] > arr[i+1]){
+//                 let temp=arr[i]
+//                 arr[i]=arr[i+1]
+//                 arr[i+1]=temp
+//                 swapped = true
+//             }
+//         }
+//     } while(swapped)
+// }
 
-const arr=[6,9,1,3,4,84,7]
+// const arr=[6,9,1,3,4,84,7]
 
-BubbleSort(arr)
-console.log(arr);  //[1,3,4,6,7,9,84]
+// BubbleSort(arr)
+// console.log(arr);  //[1,3,4,6,7,9,84]
+
+
+
