8c423330bee043f7bb4c42d77022300cfcb62559
Muhammed Arshad T P <arshadtp89@gmail.com>
2024-08-26 22:05:43 +0530

    Create an insert method in binarySearchTree

--- Tree/BinarySearchTree.js
+++ Tree/BinarySearchTree.js
@@ -13,6 +13,29 @@ class BinarySearchTree{
     isEmpty(){
         return this.root===null
     }
+    insert(value){
+        const node = new Node(value)
+        if(this.isEmpty()){
+            this.root=node
+        }else{
+            this.insertNode(this.root,node)
+        }
+    }
+    insertNode(root,node){
+        if(node.value < root.value){
+            if(root.left == null){
+                root.left=node
+            }else{
+                this.insertNode(root.left,node)
+            }
+        }else{
+            if(root.right == null){
+                root.right=node
+            }else{
+                this.insertNode(root.right , node)
+            }
+        }
+    }
 }
 
 const BST = new BinarySearchTree()
