5fe4d209823f5084fc71e9ed1ae4d8c726c0e2f5
Muhammed Arshad T P <arshadtp89@gmail.com>
2024-08-26 22:36:59 +0530

    Create an search  method in binarySearchTree

--- Tree/BinarySearchTree.js
+++ Tree/BinarySearchTree.js
@@ -1,7 +1,7 @@
 
 class Node{
     constructor(value){
-        this.value=null
+        this.value=value
         this.left=null
         this.right=null
     }
@@ -36,8 +36,31 @@ class BinarySearchTree{
             }
         }
     }
+    search(root,value){
+        if(!root){
+            return false
+        }else{
+            if(root.value === value){
+                return true
+            }else if(value < root.value){
+                return this.search(root.left,value)
+            }else{
+                return this.search(root.right,value)
+            }
+        }
+    }
+    
 }
 
 const BST = new BinarySearchTree()
 
 console.log('tree is empty',BST.isEmpty());
+
+BST.insert(12)
+BST.insert(22)
+
+
+console.log(BST.search(BST.root,10));
+console.log(BST.search(BST.root,20));
+console.log(BST.search(BST.root,12));
+console.log(BST.search(BST.root,1));
