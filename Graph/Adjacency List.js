9bc5d6c3db2ecac0e9bd9b623c79ffde23d86c59
Muhammed Arshad T P <arshadtp89@gmail.com>
2024-08-28 23:03:19 +0530

    create an adjacencyList,adjacencyMetix,AddVertex and addEdge

---
+++ Graph/Adjacency List.js
@@ -0,0 +1,9 @@
+
+
+adjacencyList={
+    'A' :['B'],
+    'B':['A','C'],
+    'C' :['B']
+}
+
+console.log(adjacencyList['B']);
---
+++ Graph/Adjacency metrix.js
@@ -0,0 +1,8 @@
+
+
+const metrix=[
+    [0,1,0],
+    [1,0,1],
+    [0,1,0]
+]
+console.log(metrix[1][0]);
