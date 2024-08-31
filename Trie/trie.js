1281471819799a2a2dd24e07262f03a47a3ed0a3
Muhammed Arshad T P <arshadtp89@gmail.com>
2024-08-31 20:36:20 +0530

    create a trie insertion,search and prefix

--- Trie/trie.js
+++ Trie/trie.js
@@ -1,2 +1,53 @@
 
 
+class TrieNode{
+    constructor(){
+        this.children={}
+        this.isEnd = false
+    }
+}
+class trie {
+    constructor(){
+        this.root = new TrieNode()
+    }
+    insert(word){
+        let currentNode = this.root
+        for(let char of word){
+            if(!currentNode.children[char]){
+                currentNode.children[char] = new TrieNode()
+            }
+            currentNode=currentNode.children[char]
+        }
+        return currentNode.isEnd=true
+    }
+    search(word){
+        let currentNode = this.root
+        for(let char of word){
+            if(!currentNode.children[char]){
+                return false
+            }
+            currentNode=currentNode.children[char]
+        }
+        return currentNode.isEnd
+    }
+    startswith(prefix){
+        let currentNode = this.root
+        for(let char of prefix){
+            if(!currentNode.children[char]){
+                return false
+            }
+            currentNode=currentNode.children[char]
+        }
+        return true
+    }
+
+}
+
+let tries = new trie()
+
+tries.insert("arshad")
+tries.insert('justin')
+tries.insert('aswin')
+console.log(tries.startswith('as'));
+console.log(tries.search('arshad'));
+console.log(tries.search("jus"));
