12f1d199ada7583d7e9dedba3ae6a22b76850dfc
Muhammed Arshad T P <arshadtp89@gmail.com>
2024-08-31 13:14:46 +0530

    create a heap sort

--- Heap/maxHeap.js
+++ Heap/maxHeap.js
@@ -23,24 +23,71 @@ class MaxHeap{
         let index = this.heap.length-1
         while(index > 0){
             let parentIdx=this.parentIndex(index)
-            if(this.heap[this.parentIdx] < this.heap[index]){
+            if(this.heap[parentIdx] < this.heap[index]){
                 this.swap(parentIdx,index)
                 index=parentIdx
             } else{
                 break;
             }
         } 
-        console.log(this.heap);
     }
 
+    extractMax(){
+        if(this.heap.length===0){
+            return null
+        }
+        if(this.heap.length===1){
+            return this.heap.pop()
+        }
+        let max = this.heap[0]
+        this.heap[0]=this.heap.pop()
+        this.heapifyDown(0)
+        return max
+    }
+    heapifyDown(index){
+        let largest = index
+        let leftIdx = this.leftchild(index)
+        let rightIdx=this.rightchild(index)
+        if(leftIdx < this.heap.length && this.heap[leftIdx] > this.heap[largest]){
+            largest = leftIdx
+        }
+        if(rightIdx < this.heap.length && this.heap[rightIdx] > this.heap[largest]){
+            largest=rightIdx
+        }
+        if(largest != index){
+            this.swap(index,largest)
+            this.heapifyDown(largest)
+        }
+    }
+
+    heapSort(){
+        let sorted=[]
+        // for(let i=0;i<arr.length;i++){
+        //     this.insert(arr[i])
+        // }
+        while(this.heap.length){
+            sorted.push(this.extractMax())
+        }
+        return sorted
+    
+    }
 }
 
 
 const heap = new MaxHeap()
-heap.insert(10)
-heap.insert(20)
-heap.insert(5)
-heap.insert(15)
+heap.insert(2)
+heap.insert(6)
+heap.insert(2)
+heap.insert(8)
 heap.insert(30)
-console.log(heap.heapifyup());
-console.log(heap.heap);
+
+// console.log(heap.extractMax());
+// console.log(heap.extractMax());
+// console.log(heap.extractMax());
+//  let arr=[2,6,2,8]
+console.log(heap.heapSort());
+
+// console.log(heap.heap);
+
+
+
